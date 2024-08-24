66223bb4899948b8949c33f3175f84cd53f0aba7
hasan <mohdhasan7867214@gmail.com>
2024-08-24 10:55:15 +0530

    updated changes to fetch from image url

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); 
+const cors = require('cors');
 
 const app = express();
 const PORT = 4000;
 
 
-app.use(cors()); 
+app.use(cors());
 
 app.get('/proxy', async (req, res) => {
     try {
-        const imageUrl = 'https://www.deeparteffects.com/images/generated/7786445c-8fbb-4710-b0df-22f4fd3a1764.jpg';
+        const imageUrl = req.query.imageURL;
+
+        if (!imageUrl) {
+            return res.status(400).send('Image URL is required');
+        }
+
+        console.log("Fetching image from........", imageUrl);
+
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
 
         res.set('Content-Type', 'image/jpeg');
         res.send(response.data);
     } catch (error) {
-        console.error('Error fetching the image:', error);
+        console.error('Error fetching the image:', error.message);
         res.status(500).send('Error fetching the image');
     }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Proxy server running on http://localhost:${PORT}`);
